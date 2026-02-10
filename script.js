190788cd8897d55ee2d37028eceefdfcba245eb4
SreejaThanigaivel <sreejasree0308@gmail.com>
2026-02-10 20:41:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-let accesscode=prompt("please enter the secret code:");
-if(accesscode!== "SreeVish38"){
-  document.documentElement.innerHTML="<h1> Access Denied</h1><p>Incorrect code</p>";
-}
 let attempts = 0;
 
 function askPassword() {
@@ -61,4 +57,42 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
+});
+const questionContainer = document.querySelector(".question-container");
+const resultContainer = document.querySelector(".result-container.yes");
+const gifResult = document.querySelector(".gif-result");
+const heartLoader = document.querySelector(".cssload-main");
+const yesBtn = document.querySelector(".js-yes-btn");
+const noBtn = document.querySelector(".js-no-btn");
+
+// change the position of no button
+noBtn.addEventListener("mouseover", () => {
+  const newX = Math.floor(Math.random() * questionContainer.offsetWidth);
+  const newY = Math.floor(Math.random() * questionContainer.offsetWidth);
+
+  noBtn.style.left = `${newX}px`;
+  noBtn.style.top = `${newY}px`;
+});
+
+// yes button functionality
+yesBtn.addEventListener("click", () => {
+  console.log("Yes button clicked");
+  questionContainer.style.display = "none";
+  heartLoader.style.display = "inherit";
+
+  setTimeout(() => {
+    console.log("Heart animation complete");
+    heartLoader.style.display = "none";
+    resultContainer.style.display = "inherit";
+    
+    if (gifResult) {
+      gifResult.play();
+    }
+    
+    // Navigate to gifts page after 2 seconds
+    setTimeout(() => {
+      console.log("Navigating to gifts.html");
+      window.location.href = "gifts.html";
+    }, 2000);
+  }, 3000);
 });
